d3ad37cc51eb10e6d06d91e5972732789a1c00b6
wcferreira <iagwanderson@gmail.com>
2016-01-07 10:43:04 -0200

    test

--- vs_ray.m
+++ vs_ray.m
@@ -18,10 +18,11 @@ X4  = ( (F + L).^(2)  - (A-L).*(C-L) )./(4*(rho.^(2)));
 %velocity
  
 T1 = X1 + X2.*((sin(theta)).^(2)) + X3.*((cos(theta)).^(2));
-T2 = sqrt( (0.5*rho).*( X2p.*((sin(theta)).^(2)) + X3p.*((cos(theta)).^(2)) ).^(2)  + X4.*(sin(2*theta)).^(2));
+T2p = sqrt( (0.5*rho).*( X2p.*((sin(theta)).^(2)) + X3p.*((cos(theta)).^(2)) ).^(2)  + X4.*(sin(2*theta)).^(2));
 
+T2 = sqrt( ( X2.*((sin(theta)).^(2)) + X3.*((cos(theta)).^(2)) ).^(2)  + X4.*(sin(2*theta)).^(2));
 v = sqrt (T1 - T2);  %the sign is MINUS for SV-wave 
-
+vp = sqrt(T1 - T2p;
 
 %derivative
 P8 = (X2 - X3).*sin(2.*theta); 
